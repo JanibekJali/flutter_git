951e73258dccb430ef6f302fb8ac0af70afac622
Mukhammadliti <mukhammadaliti02@gmail.com>
2023-02-04 17:16:23 +0600

    Commint

---
+++ lib/app/pages/home_pages.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+  const HomePages({ Key? key }) : super(key: key);
+
+  @override
+  _HomePagesState createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(children: [Container(color: Colors.red,)],),
+    );
+  }
+}
